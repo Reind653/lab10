c28cc5aa889bc2858512301c8eb20fb53703b348
Reind653 <rib13@mail.aub.edu>
2024-11-08 13:40:51 +0200

    commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
 if __name__ == "__main__":
- unittest.main()
+    unittest.main()
