08cbd745be508633a27384fcbcf3c460d3286b79
Reind653 <rib13@mail.aub.edu>
2024-11-08 15:12:34 +0200

    commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,6 @@ import unittest
 from app import greet
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World from Reind Ballout"), "Hello, World from Reind Ballout!")
 if __name__ == "__main__":
     unittest.main()
